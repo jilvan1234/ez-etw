0ec0c86d50953c4e8fb43d2c4f2d62bf92dbc584
mireaulf <mireaulf+github2@gmail.com>
2016-01-21 19:57:14 -0500

    chore(session): remove the guid from the event_parser_do

--- src/etw_test/session_test.cpp
+++ src/etw_test/session_test.cpp
@@ -20,57 +20,58 @@ static const wstring session_generic_name(L"ez_etw__session_test");
 
 TEST_CASE("a new session context is not running by default", "[session]") {
     GIVEN("consuming from a kernel session") {
-        ez_etw::session session_ctx(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
+        ez_etw::session session(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
         THEN("the session context is not running") {
-            REQUIRE_FALSE(session_ctx.is_running());
+            REQUIRE_FALSE(session.is_running());
         }
     }
     GIVEN("consumign from a generic session") {
-        ez_etw::session session_ctx(session_generic_name, false, ez_etw::log_mode::real_time);
+        ez_etw::session session(session_generic_name, false, ez_etw::log_mode::real_time);
         THEN("the session context is not running") {
-            REQUIRE_FALSE(session_ctx.is_running());
+            REQUIRE_FALSE(session.is_running());
         }
     }
 }
 
 TEST_CASE("a session context can be started to log events", "[session]") {
-    ez_etw::session session_ctx(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
+    ez_etw::session session(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
 	GIVEN("that the session was not started") {
 		THEN("trying to stop it won't do anything") {
-			REQUIRE(session_ctx.stop());
+			REQUIRE(session.stop());
 		}
 	}
 	GIVEN("that the session is started") {
-		REQUIRE(session_ctx.start());
+		REQUIRE(session.start());
 		THEN("it can be stopped") {
-			REQUIRE(session_ctx.stop());
+			REQUIRE(session.stop());
 		}
 	}
 }
 
 TEST_CASE("event parser can be added to a session context", "[session]") {
-    ez_etw::session session_ctx(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
 	auto parser = std::make_shared<test_event_parser>(test_guid0);
 	GIVEN("that the session is started") {
-		REQUIRE(session_ctx.start());
+		ez_etw::session session(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
+		REQUIRE(session.start());
 		THEN("event parsers cannot be added") {
-			REQUIRE_FALSE(session_ctx.parsers_add(parser));
+			REQUIRE_FALSE(session.parsers_add(parser));
 		}
 	}
 	GIVEN("that the session is not running") {
+		ez_etw::session session(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
 		THEN("event parser can be added") {
-			REQUIRE(session_ctx.parsers_add(parser));
+			REQUIRE(session.parsers_add(parser));
 			AND_WHEN("a parser with the same guid is added") {
-				REQUIRE_FALSE(session_ctx.parsers_add(parser));
+				REQUIRE_FALSE(session.parsers_add(parser));
 			}
-			REQUIRE(session_ctx.start());
+			REQUIRE(session.start());
 		}
 	}
 }
 
 TEST_CASE("start/stop", "[session][test]") {
-	ez_etw::session session_ctx(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
-	REQUIRE(session_ctx.start());
-	REQUIRE(session_ctx.stop());
+	ez_etw::session session(ez_etw::controller::kernel_name, false, ez_etw::log_mode::real_time);
+	REQUIRE(session.start());
+	REQUIRE(session.stop());
 
 }
