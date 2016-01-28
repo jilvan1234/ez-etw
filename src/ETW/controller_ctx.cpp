f9382229306e8d9e8c65d6467031305896c2d4b6
mireaulf <mireaulf+github2@gmail.com>
2016-01-27 23:35:30 -0500

    chore(controller_ctx): remove OS dependencies in header

--- src/ETW/controller_ctx.cpp
+++ src/ETW/controller_ctx.cpp
@@ -1,9 +1,12 @@
 #include "inc/controller_ctx.h"
 #include "inc/properties_buffer.h"
 #include <etw/status.h>
+#include <windef.h>
+#include <winbase.h>
+#include <wmistr.h>
+#include <evntrace.h>
 
 using std::wstring;
-
 using ez_etw::controller_ctx;
 using ez_etw::properties;
 using ez_etw::properties_buffer;
@@ -11,12 +14,9 @@ using ez_etw::to_status;
 using ez_etw::status;
 using ez_etw::kernel_flags;
 
-const TRACEHANDLE controller_ctx::HANDLE_DEFAULT_VALUE = 0;
-
 controller_ctx::controller_ctx(const wstring& name)
 :m_name(name), 
 m_is_kernel_logger(name == KERNEL_LOGGER_NAMEW), 
-m_handle(HANDLE_DEFAULT_VALUE),
 m_is_running(false) {
 }
 
@@ -24,17 +24,15 @@ status controller_ctx::start(const properties& props) {
     status status(status::already_exist);
     if(!m_is_running) {
         const unsigned long flags(props.get_ptr()->get_struct()->EnableFlags);
-        const bool have_coherent_flags = (!m_is_kernel_logger && flags == kernel_flags::none) ||
-            (m_is_kernel_logger && flags!= kernel_flags::none);
+        const bool have_coherent_flags = 
+			(!m_is_kernel_logger && flags == kernel_flags::none) ||
+            (m_is_kernel_logger && flags != kernel_flags::none);
         if(have_coherent_flags) {
             TRACEHANDLE handle = 0;
             properties::ptr_type::element_type* const props_buffer = props.get_ptr();
             const ULONG start_status = StartTraceW(&handle, m_name.c_str(), props_buffer->get_struct());
             status = to_status(start_status);
             m_is_running = start_status == status::success;
-            if (m_is_running) {
-                m_handle = handle;
-            }
         } else {
             status = status::invalid_argument;
         }
@@ -45,21 +43,23 @@ status controller_ctx::start(const properties& props) {
 status controller_ctx::stop() {
     status status = status::not_running;
     if(m_is_running) {
-        const properties props(log_mode::real_time); // we don't "need" log_mode for stopping the trace
-        const ULONG stop_status = StopTraceW(m_handle, m_name.c_str(), props.get_ptr()->get_struct());
-        status = to_status(stop_status);
-        m_is_running = !(stop_status == status::success);
-        if(!m_is_running) {
-            m_handle = HANDLE_DEFAULT_VALUE;
-        }
+		status = system_stop(m_name);
+        m_is_running = status != status::success;
     }
     return status;
 }
 
 bool controller_ctx::is_running() const {
-    return m_is_running;
+	return m_is_running;
 }
 
 bool controller_ctx::is_kernel_logger() const {
     return m_is_kernel_logger;
+}
+
+status controller_ctx::system_stop(const wstring& name) {
+	static properties_buffer props(ez_etw::kernel_flags::none);
+	props.zero();
+	auto status = StopTraceW(0, name.c_str(), props.get_struct());
+	return to_status(status);
 }
--- src/ETW/inc/controller_ctx.h
+++ src/ETW/inc/controller_ctx.h
@@ -1,10 +1,6 @@
 #ifndef EZETW_PRIVATE_CONTROLLER_CTX_H
 #define EZETW_PRIVATE_CONTROLLER_CTX_H
 
-#include <windef.h>
-#include <winbase.h>
-#include <wmistr.h>
-#include <evntrace.h>
 #include <string>
 #include <etw/properties.h>
 #include <etw/status.h>
@@ -16,14 +12,11 @@ namespace ez_etw {
         ez_etw::status stop();
         bool is_running() const;
         bool is_kernel_logger() const;
+		ez_etw::status system_stop(const std::wstring& name);
     private:
-        //TRACEHANDLE get_platform_handle_error();
-        TRACEHANDLE m_handle;
         const std::wstring& m_name;
         const bool m_is_kernel_logger;
         bool m_is_running;
-
-        static const TRACEHANDLE HANDLE_DEFAULT_VALUE;
     };
 }
 
